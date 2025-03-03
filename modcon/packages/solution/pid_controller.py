68053bf39d567b20d480fbe9a07889f0698cafd3
Your Name <your-email@example.com>
2025-03-02 21:42:30 -0800

    Completed PID Controller function and added comments to clear up misunderstandings

--- modcon/packages/solution/pid_controller.py
+++ modcon/packages/solution/pid_controller.py
@@ -27,10 +27,24 @@ def PIDController(
         e_int:   current integral error (automatically becomes prev_int at next iteration).
     """
 
-    # TODO: these are random values, you have to implement your own PID controller in here
-    omega = np.random.uniform(-8.0, 8.0)
-    e = np.random.random()
-    e_int = np.random.random()
+    e = theta_ref - theta_hat
+    e_deriv = (e - prev_e)/delta_t
+    e_int = prev_int + e * delta_t
+    
+
+    k_p = 5
+    k_i = 1
+    k_d = 1
+
+    # The reason that omega isn't just the derivative of the error is because omega is the derivative of the heading whereas the error isn't
+    # The actual heading of the rovot but instead it's heading's relationship to the correct heading.
+    omega = (k_p * e) + (k_i * e_int) + (k_d * e_deriv)
+
+    # In this case our u_t is equal to our omega because u_t is normally a vector including omega as one of its entries but since
+    # we assume velocity to be constant, we are simplyifying this function and simply calculating a scalar omega and setting it as our u_t.
+    # In reality, we are returning the u_t vector and a few extra values but we're just sending out the entries independently rather than in a vector.
+
+
     # Hint: print for debugging
     # print(f"\n\nDelta time : {delta_t} \nE : {np.rad2deg(e)} \nE int : {e_int} \nPrev e : {prev_e} \nU : {u} \nTheta hat: {np.rad2deg(theta_hat)} \n")
     # ---
